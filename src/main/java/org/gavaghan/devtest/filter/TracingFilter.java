542562acb0a17b668e06af09c7f4b8b8b9dca5f6
Mike Gavaghan <mike@gavaghan.org>
2020-01-13 18:49:23 -0600

    no message

--- src/main/java/org/gavaghan/devtest/filter/TracingFilter.java
+++ src/main/java/org/gavaghan/devtest/filter/TracingFilter.java
@@ -33,233 +33,233 @@ import com.itko.util.XMLUtils;
 @SuppressWarnings("deprecation")
 public class TracingFilter extends FilterBaseImpl
 {
-	/** Logger. */
-	static private final Logger LOG = LogManager.getLogger(TracingFilter.class);
+   /** Logger. */
+   static private final Logger LOG = LogManager.getLogger(TracingFilter.class);
 
-	static private final String BROKER_URL = "brokerUrl";
-	static private final String BROKER_USERNAME = "brokerUsername";
-	static private final String BROKER_PASSWORD = "brokerPassword";
-	static private final String TOPIC = "topic";
+   static private final String BROKER_URL = "brokerUrl";
+   static private final String BROKER_USERNAME = "brokerUsername";
+   static private final String BROKER_PASSWORD = "brokerPassword";
+   static private final String TOPIC = "topic";
 
-	private String mBrokerURL = "tcp://localhost:61616";
-	private String mBrokerUsername;
-	private String mBrokerPassword;
-	private String mTopic;
+   private String mBrokerURL = "tcp://localhost:61616";
+   private String mBrokerUsername;
+   private String mBrokerPassword;
+   private String mTopic;
 
-	/**
-	 * Return a JSONNull or JSONString as appropriate.
-	 * 
-	 * @param value
-	 * @return
-	 */
-	private JSONValue getStringOrNull(String value)
-	{
-		if (value == null) return new JSONNull();
+   /**
+    * Return a JSONNull or JSONString as appropriate.
+    * 
+    * @param value
+    * @return
+    */
+   private JSONValue getStringOrNull(String value)
+   {
+      if (value == null) return new JSONNull();
 
-		return new JSONString(value);
-	}
+      return new JSONString(value);
+   }
 
-	/**
-	 * Add testExec properties to the JSON object.
-	 * 
-	 * @param json
-	 * @param testExec
-	 */
-	private void addProperties(JSONObject json, TestExec testExec)
-	{
-		JSONObject properties = new JSONObject();
-		Map<String, Object> state = testExec.getAllState();
+   /**
+    * Add testExec properties to the JSON object.
+    * 
+    * @param json
+    * @param testExec
+    */
+   private void addProperties(JSONObject json, TestExec testExec)
+   {
+      JSONObject properties = new JSONObject();
+      Map<String, Object> state = testExec.getAllState();
 
-		for (String key : state.keySet())
-		{
-			String value = testExec.getStateString(key, null);
-			properties.put(key, getStringOrNull(value));
-		}
-		json.put("properties", properties);
-	}
+      for (String key : state.keySet())
+      {
+         String value = testExec.getStateString(key, null);
+         properties.put(key, getStringOrNull(value));
+      }
+      json.put("properties", properties);
+   }
 
-	/**
-	 * Publish the JSON to the configured topic.
-	 * 
-	 * @param json
-	 */
-	private void publishJSON(JSONObject json, TestExec testExec)
-	{
-		LOG.debug("ENTER - publishJSON()");
+   /**
+    * Publish the JSON to the configured topic.
+    * 
+    * @param json
+    */
+   private void publishJSON(JSONObject json, TestExec testExec)
+   {
+      LOG.debug("ENTER - publishJSON()");
 
-		String brokerUrl = testExec.parseInState(mBrokerURL);
-		String brokerUsername = testExec.parseInState(mBrokerUsername);
-		String brokerPassword = testExec.parseInState(mBrokerPassword);
-		String topicName = testExec.parseInState(mTopic);
+      String brokerUrl = testExec.parseInState(mBrokerURL);
+      String brokerUsername = testExec.parseInState(mBrokerUsername);
+      String brokerPassword = testExec.parseInState(mBrokerPassword);
+      String topicName = testExec.parseInState(mTopic);
 
-		if (LOG.isDebugEnabled())
-		{
-			LOG.debug("Broker URL: " + brokerUrl);
-			LOG.debug("Broker Username: " + brokerUsername);
-			LOG.debug("Topic: " + topicName);
-		}
+      if (LOG.isDebugEnabled())
+      {
+         LOG.debug("Broker URL: " + brokerUrl);
+         LOG.debug("Broker Username: " + brokerUsername);
+         LOG.debug("Topic: " + topicName);
+      }
 
-		LOG.debug("Creating connection factory");
-		ActiveMQConnectionFactory connFactory = new ActiveMQConnectionFactory(brokerUsername, brokerPassword, brokerUrl);
-		Connection connection = null;
-		Session session = null;
-		MessageProducer messageProducer = null;
+      LOG.debug("Creating connection factory");
+      ActiveMQConnectionFactory connFactory = new ActiveMQConnectionFactory(brokerUsername, brokerPassword, brokerUrl);
+      Connection connection = null;
+      Session session = null;
+      MessageProducer messageProducer = null;
 
-		try
-		{
-			LOG.debug("Creating connection");
-			connection = connFactory.createConnection();
-			LOG.debug("Creating session");
-			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			LOG.debug("Creating topic");
-			Topic topic = session.createTopic(topicName);
-			LOG.debug("Creating message producer");
-			messageProducer = session.createProducer(topic);
-			LOG.debug("Creating message");
-			TextMessage message = session.createTextMessage();
+      try
+      {
+         LOG.debug("Creating connection");
+         connection = connFactory.createConnection();
+         LOG.debug("Creating session");
+         session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         LOG.debug("Creating topic");
+         Topic topic = session.createTopic(topicName);
+         LOG.debug("Creating message producer");
+         messageProducer = session.createProducer(topic);
+         LOG.debug("Creating message");
+         TextMessage message = session.createTextMessage();
 
-			message.setText(json.toPrettyString());
+         message.setText(json.toPrettyString());
 
-			LOG.debug("Publishing message");
-			messageProducer.send(message);
-		}
-		catch (JMSException exc)
-		{
-			LOG.warn("Failed to publish request to a topic", exc);
-		}
-		finally
-		{
-			try
-			{
-				if (messageProducer != null) messageProducer.close();
-				if (session != null) session.close();
-				if (connection != null) connection.close();
-			}
-			catch (JMSException ignored)
-			{
-				// no-op
-			}
-		}
-	}
+         LOG.debug("Publishing message");
+         messageProducer.send(message);
+      }
+      catch (JMSException exc)
+      {
+         LOG.warn("Failed to publish request to a topic", exc);
+      }
+      finally
+      {
+         try
+         {
+            if (messageProducer != null) messageProducer.close();
+            if (session != null) session.close();
+            if (connection != null) connection.close();
+         }
+         catch (JMSException ignored)
+         {
+            // no-op
+         }
+      }
+   }
 
-	/**
-	 * Create a new TracingFilter.
-	 */
-	public TracingFilter()
-	{
-		markFilterAsGlobal(true);
-		setThreadSafe(true);
-	}
+   /**
+    * Create a new TracingFilter.
+    */
+   public TracingFilter()
+   {
+      markFilterAsGlobal(true);
+      setThreadSafe(true);
+   }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.itko.lisa.test.FilterBaseImpl#getTypeName()
-	 */
-	@Override
-	public String getTypeName()
-	{
-		return "Step Tracer";
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see com.itko.lisa.test.FilterBaseImpl#getTypeName()
+    */
+   @Override
+   public String getTypeName()
+   {
+      return "Step Tracer";
+   }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.itko.lisa.test.FilterBaseImpl#supportsDesignTimeExecution()
-	 */
-	@Override
-	public boolean supportsDesignTimeExecution()
-	{
-		return false;
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see com.itko.lisa.test.FilterBaseImpl#supportsDesignTimeExecution()
+    */
+   @Override
+   public boolean supportsDesignTimeExecution()
+   {
+      return false;
+   }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.itko.lisa.test.FilterBaseImpl#supportsDynamicResponseToFilter()
-	 */
-	@Override
-	public boolean supportsDynamicResponseToFilter()
-	{
-		return false;
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see com.itko.lisa.test.FilterBaseImpl#supportsDynamicResponseToFilter()
+    */
+   @Override
+   public boolean supportsDynamicResponseToFilter()
+   {
+      return false;
+   }
 
-	@Override
-	public ParameterList getParameters()
-	{
-		ParameterList p = new ParameterList();
-		p.addParameter(new Parameter("Broker URL", BROKER_URL, mBrokerURL, String.class));
-		p.addParameter(new Parameter("Username", BROKER_USERNAME, mBrokerUsername, String.class));
-		p.addParameter(new Parameter("Password", BROKER_PASSWORD, mBrokerPassword, String.class));
-		p.addParameter(new Parameter("Topic", TOPIC, mTopic, String.class));
-		return p;
-	}
+   @Override
+   public ParameterList getParameters()
+   {
+      ParameterList p = new ParameterList();
+      p.addParameter(new Parameter("Broker URL", BROKER_URL, mBrokerURL, String.class));
+      p.addParameter(new Parameter("Username", BROKER_USERNAME, mBrokerUsername, String.class));
+      p.addParameter(new Parameter("Password", BROKER_PASSWORD, mBrokerPassword, String.class));
+      p.addParameter(new Parameter("Topic", TOPIC, mTopic, String.class));
+      return p;
+   }
 
-	@Override
-	public void initialize(Element elem) throws TestDefException
-	{
-		mBrokerURL = XMLUtils.findChildGetItsText(elem, BROKER_URL);
-		mBrokerUsername = XMLUtils.findChildGetItsText(elem, BROKER_USERNAME);
-		mBrokerPassword = XMLUtils.findChildGetItsText(elem, BROKER_PASSWORD);
-		mTopic = XMLUtils.findChildGetItsText(elem, TOPIC);
+   @Override
+   public void initialize(Element elem) throws TestDefException
+   {
+      mBrokerURL = XMLUtils.findChildGetItsText(elem, BROKER_URL);
+      mBrokerUsername = XMLUtils.findChildGetItsText(elem, BROKER_USERNAME);
+      mBrokerPassword = XMLUtils.findChildGetItsText(elem, BROKER_PASSWORD);
+      mTopic = XMLUtils.findChildGetItsText(elem, TOPIC);
 
-		if (mBrokerURL == null) mBrokerURL = "tcp://localhost:61616";
-		if (mBrokerUsername == null) mBrokerUsername = "";
-		if (mBrokerPassword == null) mBrokerPassword = "";
-		if (mTopic == null) mTopic = "";
-	}
+      if (mBrokerURL == null) mBrokerURL = "tcp://localhost:61616";
+      if (mBrokerUsername == null) mBrokerUsername = "";
+      if (mBrokerPassword == null) mBrokerPassword = "";
+      if (mTopic == null) mTopic = "";
+   }
 
-	@Override
-	public boolean subPreFilter(TestExec testExec) throws TestRunException
-	{
-		LOG.debug("ENTER - subPreFilter()");
-		// TODO add date to JSON
-		// SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+   @Override
+   public boolean subPreFilter(TestExec testExec) throws TestRunException
+   {
+      LOG.debug("ENTER - subPreFilter()");
+      // TODO add date to JSON
+      // SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-		JSONObject json = new JSONObject();
-		json.put("type", new JSONString("trace-pre"));
-		json.put("version", new JSONString("1.0"));
+      JSONObject json = new JSONObject();
+      json.put("type", new JSONString("trace-pre"));
+      json.put("version", new JSONString("1.0"));
 
-		// add testExec
-		JSONObject testExecJSON = new JSONObject();
-		addProperties(testExecJSON, testExec);
-		json.put("testExec", testExecJSON);
+      // add testExec
+      JSONObject testExecJSON = new JSONObject();
+      addProperties(testExecJSON, testExec);
+      json.put("testExec", testExecJSON);
 
-		publishJSON(json, testExec);
+      publishJSON(json, testExec);
 
-		return false;
-	}
+      return false;
+   }
 
-	@Override
-	public boolean subPostFilter(TestExec testExec) throws TestRunException
-	{
-		LOG.debug("ENTER - subPostFilter()");
-		// TODO add date to JSON
-		//SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+   @Override
+   public boolean subPostFilter(TestExec testExec) throws TestRunException
+   {
+      LOG.debug("ENTER - subPostFilter()");
+      // TODO add date to JSON
+      //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-		JSONObject json = new JSONObject();
-		json.put("type", new JSONString("trace-post"));
-		json.put("version", new JSONString("1.0"));
+      JSONObject json = new JSONObject();
+      json.put("type", new JSONString("trace-post"));
+      json.put("version", new JSONString("1.0"));
 
-		// add testExec
-		JSONObject testExecJSON = new JSONObject();
-		addProperties(testExecJSON, testExec);
-		json.put("testExec", testExecJSON);
+      // add testExec
+      JSONObject testExecJSON = new JSONObject();
+      addProperties(testExecJSON, testExec);
+      json.put("testExec", testExecJSON);
 
-		publishJSON(json, testExec);
+      publishJSON(json, testExec);
 
-		return false;
-	}
+      return false;
+   }
 
-	@Override
-	public boolean isScopeLocal()
-	{
-		return false;
-	}
+   @Override
+   public boolean isScopeLocal()
+   {
+      return false;
+   }
 
-	@Override
-	public boolean isScopeGlobal()
-	{
-		return true;
-	}
+   @Override
+   public boolean isScopeGlobal()
+   {
+      return true;
+   }
 }
